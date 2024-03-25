331e24f3f3ce80ff46d2d188786fa5fb67d2a695
beovan <truongvangiap04@gmail.com>
2024-03-25 11:35:31 +0700

    refactor: UserManage component and add getAllUsers service

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,81 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
 
-    state = {
-
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
     }
+  }
+  /**Life cycle method
+   * Run component:
+   * 1.Run construct -> init state
+   * 2. Did mount (set state)
+   * 3. Render
+   *
+   *
+   */
 
-    componentDidMount() {
+  render() {
+    console.log("Check render: ", this.state);
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="users-containner">
+        <div className="title text-center">Manage users with Beovan</div>
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Add</th>
+              <th>Actions</th>
+            </tr>
+            <tr>
+              {arrUsers &&
+                arrUsers.map((item, index) => {
+                  console.log("beovan check map", item, index);
+                  return (
+                    <div className="divClass">
+                      <td>{item.email}</td>
+                      <td>{item.firstName}</td>
+                      <td>{item.lastName}</td>
+                      <td>{item.address}</td>
+                      <td>
+                        <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
+                        <button className="btn-delete"><i className="fas fa-trash"></i></button>
 
-    }
+                      </td>
 
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+                    </div>
+                  );
+                })}
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
