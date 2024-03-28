370daebae911e85d36ae6bedbc0fdce92764d1b0
beovan <truongvangiap04@gmail.com>
2024-03-28 22:34:47 +0700

    Refactor: language constants and add language change functionality

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,60 +1,108 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
-import logo from '../../assets/logo.svg';
+import logo from "../../assets/logo.svg";
 import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils";
+
+import { changeLanguageApp } from "../../store/actions";
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
+
   render() {
-    console.log("check props: ",this.props);
+    let language = this.props.language;
+    console.log("check language: ", language);
     return (
       <React.Fragment>
         <div className="home-header-container">
           <div className="home-header-content">
             <div className="left-content">
               <i className="fas fa-bars"></i>
-              <img className="header-logo" src={logo}/>
+              <img className="header-logo" src={logo} />
             </div>
             <div className="center-content">
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="home-header.speciality"/></b>
+                  <b>
+                    <FormattedMessage id="home-header.speciality" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="home-header.searchdoctor"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="home-header.searchdoctor" />
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="home-header.health-facility"/></b>
+                  <b>
+                    <FormattedMessage id="home-header.health-facility" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="home-header.select-room"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="home-header.select-room" />
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="home-header.doctor"/></b>
+                  <b>
+                    <FormattedMessage id="home-header.doctor" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="home-header.select-doctor"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="home-header.select-doctor" />
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="home-header.fee"/></b>
+                  <b>
+                    <FormattedMessage id="home-header.fee" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="home-header.check-health"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="home-header.check-health" />
+                </div>
               </div>
             </div>
             <div className="right-content">
               <div className="support">
                 <i className="fas fa-question-circle"></i>
-                <FormattedMessage id="home-header.support"/>
+                <FormattedMessage id="home-header.support" />
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
-
+              <div
+                className={
+                  language === LANGUAGES.VI
+                    ? "language-vi active"
+                    : "language-vi"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VN
+                </span>
+              </div>
+              <div
+                className={
+                  language === LANGUAGES.EN
+                    ? "language-en active"
+                    : "language-en"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>{" "}
+              </div>
             </div>
           </div>
         </div>
         <div className="home-header-banner">
           <div className="content-up">
-            <div className="title1"> <FormattedMessage id="banner.title1"/></div>
-            <div className="title2"><FormattedMessage id="banner.title2"/></div>
+            <div className="title1">
+              {" "}
+              <FormattedMessage id="banner.title1" />
+            </div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2" />
+            </div>
             <div className="search">
               <i className="fas fa-search"></i>
               <input type="text" placeholder="Tìm chuyên khoa khám bệnh" />
@@ -66,37 +114,49 @@ class HomeHeader extends Component {
                 <div className="icon-child">
                   <i className="fas fa-hospital"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child1"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-mobile-alt"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child2"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-procedures"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child3"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
-                <i className="fas fa-flask"></i>
+                  <i className="fas fa-flask"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child4"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-user-md"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child5"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
-                  <i class="fas fa-briefcase-medical"></i>
+                  <i className="fas fa-briefcase-medical"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child6"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6" />
+                </div>
               </div>
             </div>
           </div>
@@ -110,12 +170,13 @@ const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
     language: state.app.language,
-
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
