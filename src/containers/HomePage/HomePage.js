9bc0bc224bb869ec7fe3aa0663176a6655b5f6f8
beovan <truongvangiap04@gmail.com>
2024-03-30 10:06:22 +0700

    style: Add new sections and settings to the HomePage component

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -3,16 +3,29 @@ import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specilty from './Section/Specilty';
 import MedicalFacility from './Section/MedicalFacility';
+import OutstandingDoctor from './Section/OutStandingDoctor';
+import HandBook from './Section/HandBook';
+
 import './HomePage.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 class HomePage extends Component {
 
     render() {
-
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          };
         return (
             <div>
                 <HomeHeader/>
-                <Specilty/>
-                <MedicalFacility />
+                <Specilty settings={settings}/>
+                <MedicalFacility settings={settings}/>
+                <OutstandingDoctor settings={settings}/>
+                <HandBook settings={settings}/>
             </div>
         );
     }
--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Slider from "react-slick";
-class Specialty extends Component {
+
+class Handbook extends Component {
   render() {
-    
     return (
-      <div className="section-share section-specialty">
+      <div className="section-share section-handbook">
         <div className="section-container">
           <div className="section-header">
             <span className="title-section">
-              Chuyên khoa phổ biến
+              Cẩm nang
             </span>
             <button className="btn-section">
               Xem thêm
@@ -18,27 +18,27 @@ class Specialty extends Component {
           <div className="section-body">
             <Slider {...this.props.settings}>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 1</div>
                 </div>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 2</div>
                 </div>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 3</div>
                 </div>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 4</div>
                 </div>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 5</div>
                 </div>
                 <div className="section-customize">
-                  <div className="bg-image section-specialty" />
+                  <div className="bg-image section-handbook" />
                   <div>Cơ xương khớp 6</div>
                 </div>
             </Slider>
@@ -64,4 +64,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(Handbook);
