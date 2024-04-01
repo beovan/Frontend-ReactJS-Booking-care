0cea0cbb1d01344b6c6ffe1eb79e62665fc84dc5
{beovan} <truongvangiap04@gmail.com>
2024-04-01 15:43:36 +0700

    Fix: language display and add user info to Header component

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
 import "./Header.scss";
 import { LANGUAGES } from "../../utils";
+import { FormattedMessage } from "react-intl";
 
 class Header extends Component {
   handleChangeLanguage = (language) => {
@@ -13,8 +14,7 @@ class Header extends Component {
   };
 
   render() {
-    const { processLogout, language } = this.props;
-
+    const { processLogout, language, userInfo } = this.props;
     return (
       <div className="header-container">
         {/* thanh navigator */}
@@ -23,6 +23,10 @@ class Header extends Component {
         </div>
 
         <div className="languages">
+          <span className="welcome">
+            <FormattedMessage id="home-header.welcome" />
+             {userInfo && userInfo.firstName ? userInfo.firstName : ""} 
+          </span>
           <span
             className={
               language === LANGUAGES.VI ? "languages-vi active" : "languages-vi"
@@ -41,12 +45,10 @@ class Header extends Component {
           </span>
 
           {/* nút logout */}
-        <div className="btn btn-logout" onClick={processLogout}>
-          <i className="fas fa-sign-out-alt"></i>
+          <div className="btn btn-logout" onClick={processLogout}>
+            <i className="fas fa-sign-out-alt"></i>
+          </div>
         </div>
-        </div>
-
-        
       </div>
     );
   }
@@ -56,6 +58,7 @@ const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
     language: state.app.language,
+    userInfo: state.user.userInfo,
   };
 };
 
--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -13,7 +13,7 @@ class HomeHeader extends Component {
 
   render() {
     let language = this.props.language;
-    console.log("check language: ", language);
+    console.log("Check userinfo: ", this.props.userInfo);
     return (
       <React.Fragment>
         <div className="home-header-container">
@@ -169,6 +169,7 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    userInfo: state.user.userInfo,
     language: state.app.language,
   };
 };
