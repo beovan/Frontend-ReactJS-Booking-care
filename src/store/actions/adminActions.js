860caf6bfb50e1232539e1ac192a7c18e8eaa80b
beovan <truongvangiap04@gmail.com>
2024-04-02 19:59:35 +0700

    fix: Update UserRedux.scss and actionTypes.js, add createNewUser action

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,5 +1,5 @@
 import actionTypes from "./actionTypes";
-import { getAllCodeService } from "../../services/userService";
+import { getAllCodeService, createNewUserService } from "../../services/userService";
 
 // export const fetchGenderStart = () => ({
 //     type: actionTypes.FETCH_GENDER_START
@@ -73,7 +73,7 @@ export const fetchRoleStart = () => {
         try {
             let res = await getAllCodeService("ROLE");
             if(res && res.errCode === 0){
-                dispatch(fetchRoleSuccess(res.data));
+                dispatch(fetchRoleSuccess());
             }
             else{
                 dispatch(fetchRoleFailed());
@@ -82,6 +82,24 @@ export const fetchRoleStart = () => {
         catch(e){
             dispatch(fetchRoleFailed());
             console.log("fetchRoleStart error: ", e);
+        }
+    }
+}
+
+export const createNewUser = (data) => {
+    return async(dispatch, getState) => {
+        try{
+            let res = await createNewUserService(data);
+            if(res && res.errCode === 0){
+                dispatch(saveUserSuccess());
+            }
+            else{
+                dispatch(saveUserFailed());
+            }
+        }
+        catch(e){
+            dispatch(saveUserFailed());
+            console.log("createNewUser error: ", e);
         }
     }
 }
